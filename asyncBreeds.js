8be5d252929a2f31c318425db5332b0dd8d79df1
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-05 23:16:29 +0000

    Files that perform async stuff

---
+++ asyncBreeds.js
@@ -0,0 +1,24 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function (breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
+    callback(error, data);
+
+  });
+  // ISSUE: Attempting to return data out here will also not work.
+  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+};
+
+// we try to get the return value
+
+breedDetailsFromFile('Bombay', (err, data) => {
+  if (!err) {
+    console.log('Return Value :', data);
+  }
+});
+
+
