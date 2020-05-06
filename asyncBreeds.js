b94afdfbdd05855b35ed77fdf8f7f593e98fe19a
Lucas Wong <lucaswong.ca@gmail.com>
2020-05-06 01:13:16 +0000

    Mocha and chai testing

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -6,19 +6,27 @@ const breedDetailsFromFile = function (breed, callback) {
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
-    callback(error, data);
-
+    if (!error) {
+      callback(data);
+    } else {
+      callback(undefined);
+    }
   });
-  // ISSUE: Attempting to return data out here will also not work.
-  //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
-};
-
+}
 // we try to get the return value
 
-breedDetailsFromFile('Bombay', (err, data) => {
-  if (!err) {
-    console.log('Return Value :', data);
-  }
-});
+// breedDetailsFromFile('Bombay', (err, data) => {
+//   if (!err) {
+//     console.log('Return Value :', data);
+//   }
+// });
+
+module.exports = breedDetailsFromFile;
 
 
+    
+
+//   });
+//   // ISSUE: Attempting to return data out here will also not work.
+//   //        Currently not returning anything from here, so breedDetailsFromFile function returns undefined.
+// };
